c75dceb5f668473d7671e22f6dc2bb5beb528317
eCurrington <eli.currington@stu.daviess.kyschools.us>
2025-02-19 16:09:27 +0000

    done

--- Lab24d.java
+++ Lab24d.java
@@ -13,10 +13,40 @@ public class Lab24d
 	public static void main( String args[] ) throws IOException
 	{
 		Scanner file = new Scanner (new File("lab24d.dat"));
+		
+		//Default TicTacToe Game
+		TicTacToe Tic = new TicTacToe();
+		System.out.println("Default Game");
+		Tic.toString();
+		System.out.println("");
+
+		//Obtaining howmany lines need to be read
+		int lineNum = file.nextInt();
+		System.out.println(lineNum);
+		file.nextLine();
+
+		//Setting Tic to the new TicTacToe game that is in the file 
+		//Printing the game and the winner using toString() method
+		for(int i=0; i<lineNum; i++)
+		{
+			System.out.println("TicTacToe Game #: " + (i+1) );
+			Tic = new TicTacToe(file.nextLine());
+			Tic.toString();
+			System.out.println("");
+		}
+
+		
 
 
+		//read how many times you should read
 
+		//TicTacToe only wants this: file.nextline()
 
+		//make a tictactoe object for each game in the file
+
+		//printy the game tictactoe
+
+		//if ( a=b && b=c ) win
 
 
 
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
@@ -9,48 +9,92 @@ import static java.lang.System.*;
 
 public class TicTacToe
 {
-	private char[][] mat;
+	private char[][] mat = new char[3][3];
+	//use charAt()
 
 	public TicTacToe()
 	{
+		String game = "OXOOOXXXO";
 
+		int letter = 0;
 
+		for(int i=0; i<mat.length; i++)
+		{
+			for(int i2=0; i2<mat[0].length; i2++)
+			{
+				mat[i][i2] = game.charAt(letter);
+				letter++;
+			}
+		}
 	}
 
 	public TicTacToe(String game)
 	{
 
+		int letter = 0;
 
+		for(int i=0; i<mat.length; i++)
+		{
+			for(int i2=0; i2<mat[0].length; i2++)
+			{
+				mat[i][i2] = game.charAt(letter);
+				letter++;
+			}
 
+		}
 
 	}
 
 	public String getWinner()
 	{
+		char a,b,c;
 
+		for(int row=0; row<mat.length; row++)
+		{
+			a = mat[row][0];
+			b = mat[row][1];
+			c = mat[row][2];
 
+			if(a==b && b==c)
+			{
+				return a + "";
+			}
+		}
 
+		for(int col=0; col<mat[0].length; col++)
+		{
+			a = mat[0][col];
+			b = mat[1][col];
+			c = mat[2][col];
 
-
-
-
-
-
-
-
-
-		return "";
+			if(a==b && b==c)
+			{
+				return a + "";
+			}
+		}
+		if(mat[0][0] == mat[1][1] && mat[1][1] == mat[2][2])
+			return mat[0][0] + "";
+		if(mat[0][2] == mat[1][1] && mat[1][1] == mat[2][0])
+			return mat[0][2] + "";
+		
+		return "Tie";
 	}
 
 	public String toString()
 	{
 		String output="";
 
-
-
-
-
-
+		
+		for(int row=0; row<mat.length; row++)
+		{
+			for(int col=0; col<mat[0].length; col++)
+			{
+				System.out.print(mat[row][col]);
+			}
+			System.out.println("");
+				
+		}
+		System.out.println("Winner: " + getWinner());
 		return output+"\n\n";
 	}
 }
